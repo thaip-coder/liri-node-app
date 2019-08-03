161d636c5203af958b4e67e386e95cd97f18e425
Thai Phan <thaiphan90@yahoo.com>
2019-08-03 16:17:29 -0500

    added images and updating readme

---
+++ README.md
@@ -0,0 +1,45 @@
+1. The problem my app is trying to solve:
+    - My app is trying to provide the user with 3 functions. It can:
+        1. Search for a spotify song and display the following information:
+            - Song name
+            - Artist name
+            - Song album
+            - Link to spotify song
+        2. Search for information about a movie, displaying the following:
+            - Movie title
+            - Year movie was published
+            - IMDB rating
+            - Rotten tomatoes rating
+            - Country produced
+            - Language
+            - Plot
+            - Actors
+        3. Search concert information given an artist, displaying the following information:
+            - Name of venue
+            - Venue location
+            - Date of event
+        4. Additionally, my app can import a command from a text file and run that command utilizing the 3 functions provided. 
+
+2. How the app is organized:
+    - Firstly, the global variables are introduced
+        - This includes any required modules and process.argv's
+    - Secondly, the switch statement will be traversed when the user decides what function they wish to use and what search query they input.
+    - Lastly, the functions that are called in the switch statement are displayed.
+
+3. How to run the app:
+    1. In the command line, to intiate the app, type(without quotations):
+        - "node liri" 
+    2. After "liri" hit <space> and insert 1 of 4 commands:
+        - spotify-this-song
+            - After this command hit <space> and input the song you wish you search.
+            - Then hit <enter>.
+        - movie-this
+            - After this command hit <space> and input the movie you wish to search.
+            - Then hit <enter>.
+        - concert-this
+            - After this command hit <space> and input the artist you wish to search.
+            -Then hit <enter>.
+        - do-what-it-says
+            - Hit enter, and it will run whatever command is in the random.txt file.
+
+4. 
--- liri.js
+++ liri.js
@@ -60,16 +60,19 @@ function spotifySong() {
             console.log("Error: " + err);
             return;
         };
+        console.log("====================");
         console.log("\nSong Name: " + results.tracks.items[0].name);
         console.log("Artist(s): " + results.tracks.items[0].artists[0].name);
         console.log("Song Album: " + results.tracks.items[0].album.name);
         console.log("Song Link: " + results.tracks.items[0].external_urls.spotify);
+        console.log("\n====================");
     });
 };
 
 //Movie search function: Title, Year, IMBD rating, Rotten Tomatoes Rating, Country Produced, Language, Plot, Actors
 function movieThis() {
     axios.get(movieURL).then(function(response) {
+        console.log("====================");
         console.log("\nMovie Title: " + response.data.Title);
         console.log("Year Released: " + response.data.Year);
         console.log("IMDB rating: " + response.data.imdbRating);
@@ -77,17 +80,20 @@ function movieThis() {
         console.log("Country/Countries Shot In: " + response.data.Country);
         console.log("Languages: " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.datad.Actors);
+        console.log("Actors: " + response.data.Actors);
+        console.log("\n====================");
     });
 };
 
 //Concert search function: Name of venue, venue location, date of the event
 function concertThis() {
     axios.get(concertURL).then(function(response) {
+        console.log("====================");
         console.log("\nBand Playing: " + search);
         console.log("Venue Name: " + response.data[0].venue.name);
         console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
         console.log("Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        console.log("\n====================");
     });                                                                                                                                                 
 };
 
